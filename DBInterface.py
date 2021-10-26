fccaa338e857edb8f47cf56fb4237b48b5c2848a
Andreas Mütter <andreas.muetter@ph.tum.de>
2021-10-26 21:40:42 +0200

    basic functionality

--- DBInterface.py
+++ DBInterface.py
@@ -7,3 +7,9 @@ def get_db(database):
     if db is None:
         db = g._database = sqlite3.connect(database)
     return db
+
+def get_tables(conn):
+    tables = []
+    for row in conn.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall():
+        tables.append(row[0])
+    return tables
